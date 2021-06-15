b94945b27d8c02218f5725733b6b34c8805d02f8
Asku_SU <zaharandrei15@mail.ru>
2021-06-16 00:16:18 +0300

    Решил первую задачу

--- HW_L6.cpp
+++ HW_L6.cpp
@@ -1,11 +1,66 @@
 ﻿
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+unsigned short GetUserInput()
+{
+	while (true)
+	{
+		int input;
+		cin >> input;
+		if (cin.fail() || input <= 0)
+		{
+			cout << "Ошибка ввода, повторите ввод: ";
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		}
+		else
+		{
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			return (unsigned short)input;
+		}
+	}
+}
+
+void PrintArr(size_t size, bool NeedCnt, unsigned int* Arr)
+{
+	if (NeedCnt) cout << "Кол-во элементов = " << size << endl;
+	for (size_t i = 0; i < size; i++)
+	{
+		cout << Arr[i] << ' ';
+	}
+	cout << endl;
+}
+
 void Task1()
 {
+	cout << "Введите размер массива N = ";
+	unsigned short n = 0;
 
+	do
+	{
+		if (n != 0) cout << "В беззнаковый int не влезает степень 2ки больше 32х, повторите ввод\nN = ";
+		n = GetUserInput();			
+	} while (n > 32);
+	
+	unsigned int* ptrArr = new (nothrow) unsigned int[n];
+	
+	if (ptrArr)
+	{
+		for (size_t i = 0; i < n; i++)
+		{
+			ptrArr[i] = pow(2, i);
+		}
+		PrintArr(n, false, ptrArr);
+		delete[] ptrArr;
+	}
+	else
+	{
+		cout << "Ошибка выделения памяти!";
+	}
 }
 
 void Task2()
@@ -32,13 +87,13 @@ int main()
 {
 	setlocale(LC_ALL, "RU");
 
-	int taskNumber = 0;
+	unsigned short taskNumber = 0;
 	while (true)
 	{
 		do
 		{
 			cout << "Введите номер задачи от 1 до 5:" << endl;
-			cin >> taskNumber;
+			taskNumber = GetUserInput();
 		} while (taskNumber > 5 || taskNumber < 1);
 
 		cout << endl;
